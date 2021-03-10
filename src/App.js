c4af855cb841fb53854872d4e051ee36ba6d5708
Audrey <audreyuu@gmail.com>
2021-03-09 23:26:46 -0500

    add reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
 const stateMachine = {
-  initial: {},
-  loadingModel: {},
-  awaitingUpload: {},
-  ready: {},
-  classifying: {},
-  complete: {},
+  initial: 'initial',
+  states: {
+    initial: { on: { next: 'loadingModel' } },
+    loadingModel: { on: { next: 'awaitingUpload' } },
+    awaitingUpload: { on: { next: 'ready' } },
+    ready: { on: { next: 'classifying' } },
+    classifying: { on: { next: 'complete' } },
+    complete: { on: { next: 'awaitingUpload' } }
+  }
 }
+
+const reducer = (currentState, event) => stateMachine.states[currentState].on[event] || stateMachine.initial;
 
 function App() {
   return (
