5c16819fb6ce21043629570742701387c481b73d
Audrey <audreyuu@gmail.com>
2021-03-14 23:13:20 -0400

    add styling file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useState, useRef } from 'react';
 import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as tf from '@tensorflow/tfjs'
 import './App.css';
 
 const stateMachine = {
@@ -17,14 +18,18 @@ const stateMachine = {
 const reducer = (currentState, event) => stateMachine.states[currentState].on[event] || stateMachine.initial;
 
 function App() {
+  tf.setBackend("cpu"); 
+
   const [state, dispatch] = useReducer(reducer, stateMachine.initial);
   const [model, setModel] = useState(null);
   const [imageURL, setImageURL] = useState(null);
+  const [results, setResults] = useState([]);
   const inputRef = useRef();
+  const imageRef = useRef();
 
   const next = () => dispatch('next')
 
-  const loadModel = async () = => {
+  const loadModel = async () => {
     const mobilenetModel = await mobilenet.load();
     setModel(mobilenetModel);
   }
@@ -38,18 +43,28 @@ function App() {
     }
   }
 
+  const identify = async () => {
+    next();
+    const results = await model.classify(imageRef.current);
+    console.log({results})
+    next();
+  }
+
   const buttonProps = {
-    initial: { text: 'Load Model', action: () => {} }, 
+    initial: { text: 'Load Model', action: loadModel }, 
     loadingModel: { text: 'Loading Model', action: () => {} }, 
-    awaitingUpload: { text: 'Upload Photo', action: () => {} }, 
+    awaitingUpload: { text: 'Upload Photo', action: () => inputRef.current.click() }, 
     ready: { text: 'Identify', action: () => {} }, 
     classifying: { text: 'Identifying', action: () => {} }, 
     complete: { text: 'Reset', action: () => {} }
   }
 
+  const { showImage = false } = stateMachine.states[state];
+
   return (
     <div>
-      <input type='file' accept='image/*' capture='camera' ref={inputRef} />
+      {showImage && <img src={imageURL} alt='upload-preview' ref={imageRef} />}
+      <input type='file' accept='image/*' capture='camera' ref={inputRef} onChange={handleUpload} />
       <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
     </div>
   );
