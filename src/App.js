354a497769dad6b89167da26b66737bcf0e00bb1
Audrey <audreyuu@gmail.com>
2021-03-14 21:36:18 -0400

    add button props

--- src/App.js
+++ src/App.js
@@ -17,10 +17,20 @@ const reducer = (currentState, event) => stateMachine.states[currentState].on[ev
 
 function App() {
   const [state, dispatch] = useReducer(reducer, stateMachine.initial);
+  const next = () => dispatch('next')
+
+  const buttonProps = {
+    initial: { text: 'Load Model', action: () => {} }, 
+    loadingModel: { text: 'Loading Model', action: () => {} }, 
+    awaitingUpload: { text: 'Upload Photo', action: () => {} }, 
+    ready: { text: 'Identify', action: () => {} }, 
+    classifying: { text: 'Identifying', action: () => {} }, 
+    complete: { text: 'Reset', action: () => {} }
+  }
 
   return (
     <div>
-      <button onClick={() => dispatch('next')}>{state}</button>
+      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
     </div>
   );
 }
